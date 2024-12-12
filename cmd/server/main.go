c70efb11dd289b2ca126d72163b2cb189ba50554
Tanya <tanya1515@ispras.ru>
2024-12-12 17:34:02 +0300

    change variable names

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,32 +16,32 @@ func ValidateRequest(next http.Handler) http.Handler {
 			return
 		}
 
-		metric_type := r.PathValue("metric_type")
+		metricType := r.PathValue("metricType")
 
-		if (metric_type != "counter") && (metric_type != "gauge") {
-			http.Error(rw, fmt.Sprintf("Error 400: Invalid metric type: %s", metric_type), http.StatusBadRequest)
+		if (metricType != "counter") && (metricType != "gauge") {
+			http.Error(rw, fmt.Sprintf("Error 400: Invalid metric type: %s", metricType), http.StatusBadRequest)
 			return
 		}
 
-		metric_name := r.PathValue("metric_name")
-		if metric_name == "" {
+		metricName := r.PathValue("metricName")
+		if metricName == "" {
 			http.Error(rw, "Error 404: Metric name was not found", http.StatusNotFound)
 			return
 		}
 
-		metric_value := r.PathValue("metric_value")
+		metricValue := r.PathValue("metricValue")
 
-		if metric_type == "counter" {
-			_, err := strconv.ParseInt(metric_value, 10, 64)
+		if metricType == "counter" {
+			_, err := strconv.ParseInt(metricValue, 10, 64)
 			if err != nil {
-				http.Error(rw, fmt.Sprintf("Error 400: Invalid metric value: %s", metric_value), http.StatusBadRequest)
+				http.Error(rw, fmt.Sprintf("Error 400: Invalid metric value: %s", metricValue), http.StatusBadRequest)
 				return
 			}
 		}
-		if metric_type == "gauge" {
-			_, err := strconv.ParseFloat(metric_value, 64)
+		if metricType == "gauge" {
+			_, err := strconv.ParseFloat(metricValue, 64)
 			if err != nil {
-				http.Error(rw, fmt.Sprintf("Error 400: Invalid metric value: %s", metric_value), http.StatusBadRequest)
+				http.Error(rw, fmt.Sprintf("Error 400: Invalid metric value: %s", metricValue), http.StatusBadRequest)
 				return
 			}
 		}
@@ -52,20 +52,20 @@ func ValidateRequest(next http.Handler) http.Handler {
 
 func ProcessMetrix(rw http.ResponseWriter, r *http.Request) {
 	//save metrics
-	metric_type := r.PathValue("metric_type")
+	metricType := r.PathValue("metricType")
 
-	metric_name := r.PathValue("metric_name")
+	metricName := r.PathValue("metricName")
 
-	metric_value := r.PathValue("metric_value")
+	metricValue := r.PathValue("metricValue")
 
-	if metric_type == "counter" {
-		metric_value_int64, _ := strconv.ParseInt(metric_value, 10, 64)
-		Storage.StorageAddCounterValue(metric_name, metric_value_int64)
+	if metricType == "counter" {
+		metricValueInt64, _ := strconv.ParseInt(metricValue, 10, 64)
+		Storage.StorageAddCounterValue(metricName, metricValueInt64)
 	}
 
-	if metric_type == "gauge" {
-		metric_value_float64, _ := strconv.ParseFloat(metric_value, 64)
-		Storage.StorageAddGaugeValue(metric_name, metric_value_float64)
+	if metricType == "gauge" {
+		metricValueFloat64, _ := strconv.ParseFloat(metricValue, 64)
+		Storage.StorageAddGaugeValue(metricName, metricValueFloat64)
 	}
 
 	rw.Header().Set("Content-Type", "text/plain")
@@ -81,7 +81,7 @@ func main() {
 	mux := http.NewServeMux()
 	// Handle registers the handler for the given pattern in Router/mux
 	// otherwise HandleFunc registers the handler function for the given pattern in
-	mux.Handle(`/update/{metric_type}/{metric_name}/{metric_value}`, ValidateRequest(http.HandlerFunc(ProcessMetrix)))
+	mux.Handle(`/update/{metricType}/{metricName}/{metricValue}`, ValidateRequest(http.HandlerFunc(ProcessMetrix)))
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
